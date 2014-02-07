c4ccf2987c5cab01be67fe80d59a50a233cedc10
Krasimir Kirilov <kkirilov91@gmail.com>
2014-02-07 11:46:13 +0200

    add current state of hitman

---
+++ board.js
@@ -0,0 +1,78 @@
+var canvas=document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+
+var points = [];
+var boxSize = 50;
+var soldierSize = 20;
+var startX = 0;
+var startY = 0;
+var stepMove = 20;
+var cx = startX,
+cy = startY;
+
+var player = new Player();
+
+var isPlayerAlive = true;
+
+generateBoard();
+var numberOfSoldiers = 4;
+var soldiers = [];
+
+for(i=0; i<numberOfSoldiers; i++){
+  soldiers[i] = new SoldierAI();
+}
+
+player.start();
+soldiers.forEach(function(soldier){
+  soldier.startAI();
+});
+
+function restart() {
+  isPlayerAlive = false;
+
+  setTimeout(function(){
+    points = []; 
+    cx = startX;
+    cy = startY;
+    ctx.clearRect(0, 0, 1000, 1000);
+    isPlayerAlive = true;
+    generateBoard();
+
+    player.restart();
+    
+    soldiers.forEach(function(soldier){
+      soldier.restartAI();
+    });
+  },300);
+}
+
+function generateBoard() {
+  for(var x=30; x<=930; x+=100){
+    var y=30;
+    while(y<330){
+      var rnd = generateRandomNumber(0,100);
+      ctx.fillStyle="#FF0000";
+      ctx.fillRect(x,y+rnd,boxSize,boxSize);
+      point = {};
+      point.x = x;
+      point.y = y+rnd;
+      points.push(point);
+      y = y + boxSize + rnd;
+    }
+  }
+  ctx.fillStyle = "#00FF00";
+  ctx.fillRect(startX, startY, soldierSize, soldierSize);
+}
+ 
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
---
+++ soldier.js
@@ -0,0 +1,168 @@
+var soldierSeenAtRegion = [0,0,0,0,0,0,0,0];
+
+var directions = {
+	LEFT : 0,
+	RIGHT : 1,
+	UP : 2,
+	DOWN : 3,
+};
+
+var NOT_SEEN = {x: -10, y: -10};
+
+
+var SoldierAI = function(x, y, color, size){
+	//default values
+	x = typeof x !== "undefined" ? x : 900;
+	y = typeof y !== "undefined" ? y : 400;
+	color = typeof color !== "undefined" ? color : "#0000FF";
+	size = typeof size !== "undefined" ? size : 20;
+
+	var currX = x;
+	var currY = y;
+	var seen = false;
+	var soldierColor = color;
+	var soldierSize = size;
+	var sightValidator = new SightValidator();
+	this.visitedPositions = null;
+	
+	//private
+	
+	var soldierAction = function() {
+		if (!isPlayerAlive) {
+			return;
+		}
+		setTimeout(function(){
+			
+			var seenAt = sightValidator.lookForOpponent(currX, currY);
+			
+			//TODO: create const coords for unseen 
+			if (seenAt.x === NOT_SEEN.x && seenAt.y === NOT_SEEN.y) {
+				searchOpponent();
+				//TODO: underquestioning:
+				seen = false;
+			} else {
+				if(!seen) {
+					markSoldierSeenPosition(seenAt.x, seenAt.y);
+					seen = true;
+				}
+				fightOpponent(seenAt);
+			}
+
+			soldierAction();
+		},100);
+	}
+
+	var fightOpponent = function(opponentCoords){
+		
+		if (MoveValidator.isValidDirectedMove(currX, currY, opponentCoords.direction)){
+			ctx.fillStyle = soldierColor;
+			moveWithDirection(currX, currY, opponentCoords.direction);
+		}
+
+		Shooting.shoot(currX, currY, opponentCoords.direction);
+	};
+	
+	var searchOpponent = function() {
+		var availablePos = [];
+		// alert(visitedPositions[currX-stepMove][currY]);
+		if(MoveValidator.isValidLeftMove(currX,currY) && !visitedPositions[currX - stepMove][currY])
+			availablePos.push({x:currX - stepMove, y:currY}); 
+		if(MoveValidator.isValidRightMove(currX,currY) && !visitedPositions[currX + stepMove][currY])
+			availablePos.push({x:currX + stepMove, y:currY}); 
+		if(MoveValidator.isValidUpMove(currX,currY) && !visitedPositions[currX][currY - stepMove])
+			availablePos.push({x:currX, y:currY - stepMove}); 
+		if(MoveValidator.isValidDownMove(currX,currY) && !visitedPositions[currX][currY + stepMove])
+			availablePos.push({x:currX, y:currY + stepMove});
+
+		if (availablePos.length == 0) {
+			if(MoveValidator.isValidLeftMove(currX,currY))
+				availablePos.push({x:currX - stepMove, y:currY}); 
+			if(MoveValidator.isValidRightMove(currX,currY))
+				availablePos.push({x:currX + stepMove, y:currY}); 
+			if(MoveValidator.isValidUpMove(currX,currY))
+				availablePos.push({x:currX, y:currY - stepMove}); 
+			if(MoveValidator.isValidDownMove(currX,currY))
+				availablePos.push({x:currX, y:currY + stepMove});			
+		}
+
+		var rndIndex = generateRandomNumber(0, availablePos.length-1);
+		var nextMove = availablePos[rndIndex];
+		
+		ctx.fillStyle = soldierColor;
+		move(currX, currY, nextMove.x, nextMove.y);
+
+		currX = nextMove.x;
+		currY = nextMove.y;
+
+		markAsVisited(currX, currY);
+	}
+
+	var create2DArray = function(rows) {
+	  var arr = [];
+	  for (var i=0;i<rows;i++) {
+	     arr[i] = [];
+	  }
+	  return arr;
+	}
+
+	var markSoldierSeenPosition = function (x, y) {
+		if(x<250)	
+			if(y < 240)
+				soldierSeenAtRegion[0]++;
+			else
+				soldierSeenAtRegion[1]++;
+		else if(x < 500)
+				if(y < 240)
+					soldierSeenAtRegion[2]++;
+				else
+					soldierSeenAtRegion[3]++;
+		else if(x < 750)
+				if (y < 240)
+					soldierSeenAtRegion[4]++;
+				else
+					soldierSeenAtRegion[5]++;
+		else if(y < 240)
+			soldierSeenAtRegion[6]++;
+		else 
+			soldierSeenAtRegion[7]++;
+	}
+
+
+	function markAsVisited(currX, currY) {
+		for (var i=currX; i < currX+soldierSize; i++) {
+			for (var j=currY; j < currY + soldierSize; j++) {
+				visitedPositions[i][j] = 1;
+			}
+		}		
+	}
+
+	function start(){
+		visitedPositions = create2DArray(1000);
+		ctx.fillStyle = soldierColor;
+		ctx.fillRect(currX, currY, soldierSize, soldierSize);
+		markAsVisited(currX, currY);
+		soldierAction();
+	}
+	///public
+
+	//Getters
+	this.isSeen = function() { return seen; }
+	this.getX = function() { return currX; }
+	this.getY = function() { return currY; }
+	this.getSoldierColor = function() { return soldierColor; }
+	this.getSoldierSize = function() { return soldierSize; }
+
+	this.startAI = function(){
+		start();
+	}
+
+	this.restartAI = function() {
+		currX = x;
+		currY = y;
+		soldierSize = size;
+		soldierColor = color;
+
+		start();
+	}
+
+}
